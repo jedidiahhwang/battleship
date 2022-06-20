0c061e91fc139c52f4e3c79ae7951cf3ef532249
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-06-20 07:42:45 -0600

    first attempt at checkYoruself() implementation

--- src/SimpleStartup.java
+++ src/SimpleStartup.java
@@ -3,13 +3,30 @@ public class SimpleStartup {
     private int numOfHits = 0;
 
     public String checkYourself(int guess) {
-        for()
+        // Instantiate appropriate string.
+        String result = "miss";
+
+        // GET the user's guess.
+        int userGuess = guess;
+
+        // REPEAT with each cell in the int array.
+        for(int cell: locationCells) {
+            if(cell == guess) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+
+        // CHECK to see if the numOfHits equals cells length for a win.
+        if(numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result);
     }
 
     public void setLocationCells(int[] locations) {
-        // Construct for each loop
-        for(int cell: locationCells) {
 
-        }
     }
 }
